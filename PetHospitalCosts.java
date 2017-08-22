c8082f6770d5e238f2326a7788bda4a63118e0b7
Kim Lew <kimlew451@gmail.com>
2017-08-22 15:04:56 -0600

    Added type to (int numNights)

--- PetHospitalCosts.java
+++ PetHospitalCosts.java
@@ -16,14 +16,13 @@ Ask user if they want to calculate the charges for another patient
  */
 import java.util.Scanner;
 
-
 public class PetHospitalCosts {
-  int numNights = 0;
+  //int numNights = 0;
 
   public static void main(String[] args) {
     System.out.println("Type number of overnight stays required: ");
     Scanner in = new Scanner(System.in);
-    numNights = in.nextInt();
+    int numNights = in.nextInt();
     
     double overnightCharges = calcOvernightCharges(numNights);
     double medCharges = calcMedicationCharges();
@@ -31,7 +30,7 @@ public class PetHospitalCosts {
     double totalCharge = overnightCharges + medCharges + labServCharges;
   }
   
-  public static double calcOvernightCharges(numNights) {
+  public static double calcOvernightCharges(int numNights) {
     double cleanBedding = 10.50;
     double cleanRoom = 50.00;
     
