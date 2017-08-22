f32a237d10da9280644626642b6fc84149c2815e
Kim Lew <kimlew451@gmail.com>
2017-08-22 14:03:19 -0600

    Initial commit

---
+++ PetHospitalCosts.java
@@ -0,0 +1,29 @@
+/* Pet patient hospital stay calculation
+Program that computes and displays total charges for pet patient 
+hospital stay
+
+Overnight stay, have:
+- hospital overnight charges
+- medication charges
+- lab service charges
+
+Not an overnight stay, have:
+- medication charges 
+- lab service charges
+
+Calculate the charges
+Print the final total
+Ask user if they want to calculate the charges for another patient
+ */
+package pethospitalstay;
+import java.util.Scanner;
+
+
+public class PetHospitalCosts {
+
+    public static void main(String[] args) {
+        
+    }
+    
+} // End of: class
+
